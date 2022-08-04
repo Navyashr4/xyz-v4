8819ac4942cb9c089b2cc2a6f5706ace98374797
Navya Shree <navyashr4@gmail.com>
2022-08-04 17:27:26 +0530

    making formulae dynamic

--- src/components/AnalysisResult.js
+++ src/components/AnalysisResult.js
@@ -1,48 +1,59 @@
 import React from "react";
+import formula from './Formula.js'
 
-const ResultDeclaration = () => {
+
+const ResultDeclaration = ({loss, invType, invObjective}) => {
+  let word = 'LOSS';
+  let multiplier = 1;
+  if (loss < 0){
+    word = 'PROFIT';
+    multiplier = -1; 
+  }
   return (
     <div className="text-center">
       <div className="text-indigo-600 font-medium mb-4">
-        ESTIMATED LOSS FROM THIS INVESTMENT:
+        ESTIMATED {word} FROM THIS INVESTMENT:
       </div>
       <div className="text-[32px] font-bold mb-3">
-        Rs. 10,929
+        Rs. {loss * multiplier}
       </div>
-      <div className="text-gray-500 px-10 text-centre tracking-[1%]">
-      As the supporting data below shows, inflation rates have been much higher than bank interest rates historically. Hence its not advisable to put money in Bank Fixed Deposit if your objective is saving for retirement.
-      </div>
+      {(loss !== 0) ? loss > 0 ? <div className="text-gray-500 px-10 text-centre tracking-[1%]">
+      As the supporting data below shows, inflation rates have been much higher than bank interest rates historically. Hence its not advisable to put money in {invType} if your objective is {invObjective}.
+      </div> : <div className="text-gray-500 px-10 text-centre tracking-[1%]">You made a <span className="text-indigo-600">profit!</span> Great investment! </div> : <div className="text-gray-500 px-10 text-centre tracking-[1%]">No loss! Good investment.</div>}
     </div>
   );
 };
 
-const Parameters = () => {
+const Parameters = ({principal, interestEarned, interest, period, earningsNeeded, maturityValue}) => {
   return (
     <div className="text-center">
       <div className="text-lg mb-4">
-        <span className="text-gray-400 mr-2">Invested amount is</span>Rs.67822
+        <span className="text-gray-400 mr-2">Invested amount is</span>Rs. {principal}
       </div>
       <div className="text-lg mb-4">
-        <span className="text-gray-400 mr-2">Total Compunded interest earned at 9% is</span>Rs.67822
+        <span className="text-gray-400 mr-2">Total Compound interest earned at {interest}% is</span>Rs. {interestEarned}
       </div>
       <div className="text-lg mb-4">
-        <span className="text-gray-400 mr-2">Total maturity value after 3 years is</span>Rs.67822
+        <span className="text-gray-400 mr-2">Total maturity value after {period} years is</span>Rs. {maturityValue}
       </div>
       <div className="text-lg mb-4">
-        <span className="text-gray-400 mr-2">Earnings needed to beat 7% inflation is</span>Rs.67822
+        <span className="text-gray-400 mr-2">Earnings needed to beat 7% inflation is</span>Rs. {earningsNeeded}
       </div>
 
     </div>
   );
 };
 
-const AnalysisResult = () => {
+const AnalysisResult = ({principal, period, interest, invType, invObjective}) => {
+  const result = formula(principal, period, interest, invType);
+  console.log("result", result);
+
   return (
     <div className="px-4">
         <p className="text-gray-900 text-3xl font-semibold mb-8 text-center">Your investment analysis report</p>
-        <Parameters/>
+        <Parameters principal = {principal} interestEarned = {result.interestEarned} interest={interest} period={period} maturityValue={result.maturityValue} earningsNeeded={result.earningsNeeded} />
         <div className="p-5"></div>
-        <ResultDeclaration />
+        <ResultDeclaration loss={result.loss} invType={invType} invObjective={invObjective}/>
     </div>
   );
 };
--- src/components/CalcForm.js
+++ src/components/CalcForm.js
@@ -23,12 +23,12 @@ const CalcForm = () => {
   // customising placeholder text based on inv type chosen
 
   useEffect(() => {
-    switch(invType){
-      case 'Bank Fixed Deposit' : {setPlaceholderIdx(1); setShowInputs(true); break;}
-      case 'Gold' : {setPlaceholderIdx(2); setShowInputs(true); break;}
-      case 'Real Estate' : {setPlaceholderIdx(3); setShowInputs(true); break;}
-      case 'Guaranteed Income Plan' : {setPlaceholderIdx(4); setShowInputs(true); break;}
-      default : {setPlaceholderIdx(0); setShowInputs(false);}
+    switch (invType) {
+      case 'Bank Fixed Deposit': { setPlaceholderIdx(1); setShowInputs(true); break; }
+      case 'Gold': { setPlaceholderIdx(2); setShowInputs(true); break; }
+      case 'Real Estate': { setPlaceholderIdx(3); setShowInputs(true); break; }
+      case 'Guaranteed Income Plan': { setPlaceholderIdx(4); setShowInputs(true); break; }
+      default: { setPlaceholderIdx(0); setShowInputs(false); }
     }
   }, [invType]);
 
@@ -37,6 +37,46 @@ const CalcForm = () => {
     periodPlaceholder,
     interestPlaceholder,
   } = calcFormPlaceholders[placeholderIdx];
+
+  const resetState = (event) => {
+    event.preventDefault();
+    setAnalyse(false);
+    setInvType('I have invested in');
+    setPrincipal(0);
+    setPeriod(0);
+    setInterest(0);
+    setInvObjective();
+  }
+
+  const isValid = () => {
+    let isValid;
+    if (invType === undefined) {
+      isValid = false;
+      alert("Choose type of investment");
+    } else if (principal === 0 || principal === undefined) {
+      isValid = false;
+      alert("Enter amount invested in Rupees");
+    } else if (period === 0 || period === undefined) {
+      isValid = false;
+      alert("Enter investment period in years");
+    } else if (interest === 0 || interest === undefined) {
+      isValid = false;
+      alert("Enter interest rate in %");
+    } else if (invObjective === undefined) {
+      isValid = false;
+      alert("Choose investment objective");
+    } else {
+      isValid = true;
+    }
+    return isValid;
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    if (isValid()) {
+      setAnalyse(true);
+    }
+  }
 
   const [showPrincipalLabel, setShowPrincipalLabel] = useState(false);
   console.log("showPrincipalLabel", showPrincipalLabel);
@@ -50,7 +90,7 @@ const CalcForm = () => {
       data-aos-offset="500"
     >
       <form className="flex flex-col items-center space-y-12">
-        <select 
+        <select
           className="select w-[90%] max-w-[300px] text-center text-[16px] md:text-md"
           value={invType}
           onChange={(e) => setInvType(e.target.value)}>
@@ -60,64 +100,75 @@ const CalcForm = () => {
           <option value="Real Estate">Real estate</option>
           <option value="Guaranteed Income Plan">Guaranteed Income Plan</option>
         </select>
-        
-        {showInputs &&
-        <div className="text-center w-[90%] max-w-[300px] placeholder:text-darkblue placeholder:text-center">
-          <label className="text-sm text-indigo-600">Principal Amount</label>
-          <input 
-            className="input text-center placeholder:text-[16px] md:placeholder:text-md"
-            type="number"
-            value={principal}
-            placeholder={amountPlaceholder}
-            onChange={(e) => setPrincipal(e.target.value)}>
-          </input>
-        </div>}
 
         {showInputs &&
-        <div className="text-center w-[90%] max-w-[300px] placeholder:text-darkblue placeholder:text-center">
-          <label className="text-sm text-indigo-600">Interest (%)</label>
-          <input 
-            className="input text-center placeholder:text-[16px] md:placeholder:text-md"
-            type="number"
-            value={interest}
-            placeholder={interestPlaceholder}
-            onChange={(e) => setInterest(e.target.value)}>
-          </input>
-        </div>}
+          <div className="text-center w-[90%] max-w-[300px] placeholder:text-darkblue placeholder:text-center">
+            <label className="text-sm text-indigo-600">Principal Amount</label>
+            <input
+              className="input text-center placeholder:text-[16px] md:placeholder:text-md"
+              type="number"
+              value={principal}
+              placeholder={amountPlaceholder}
+              onChange={(e) => setPrincipal(e.target.value)}>
+            </input>
+          </div>}
 
         {showInputs &&
-        <div className="text-center w-[90%] max-w-[300px] placeholder:text-darkblue placeholder:text-center">
-          <label className="text-sm text-indigo-600">Investment Period (years)</label>
-          <input 
-            className="input text-center placeholder:text-[16px] lg:placeholder:text-md"
-            type="number"
-            value={period}
-            placeholder={periodPlaceholder}
-            onChange={(e) => setPeriod(e.target.value)}>
-          </input>
-        </div>}
+          <div className="text-center w-[90%] max-w-[300px] placeholder:text-darkblue placeholder:text-center">
+            <label className="text-sm text-indigo-600">Interest (%)</label>
+            <input
+              className="input text-center placeholder:text-[16px] md:placeholder:text-md"
+              type="number"
+              value={interest}
+              placeholder={interestPlaceholder}
+              onChange={(e) => setInterest(e.target.value)}>
+            </input>
+          </div>}
 
-        {showInputs && 
-        <select 
-          className="select w-[90%] max-w-[300px] text-center text-[16px] lg:text-md"
-          onChange={(e) => setInvObjective(e.target.value)}>
-          <option value="default">Investment objective</option>
-          <option value="saving for retirement">Saving for retirement</option>
-          <option value="saving for your kids' education">My kids education</option>
-          <option value="saving to buy house">Saving to buy a house</option>
-          <option value="saving to buy a car">Saving to buy a car</option>
-        </select>}
+        {showInputs &&
+          <div className="text-center w-[90%] max-w-[300px] placeholder:text-darkblue placeholder:text-center">
+            <label className="text-sm text-indigo-600">Investment Period (years)</label>
+            <input
+              className="input text-center placeholder:text-[16px] lg:placeholder:text-md"
+              type="number"
+              value={period}
+              placeholder={periodPlaceholder}
+              onChange={(e) => setPeriod(e.target.value)}>
+            </input>
+          </div>}
 
-        {showInputs && <button className="btn text-white px-8 flex justify-center w-[60vw] max-w-[300px]">
-          Calculate
-        </button>}
+        {showInputs &&
+          <select
+            className="select w-[90%] max-w-[300px] text-center text-[16px] lg:text-md"
+            onChange={(e) => setInvObjective(e.target.value)}>
+            <option value="default">Investment objective</option>
+            <option value="saving for retirement">Saving for retirement</option>
+            <option value="saving for your kids' education">My kids education</option>
+            <option value="saving to buy house">Saving to buy a house</option>
+            <option value="saving to buy a car">Saving to buy a car</option>
+          </select>}
+
+        {showInputs &&
+          <button
+            className="btn text-white px-8 flex justify-center w-[60vw] max-w-[300px]"
+            onClick={(event) => handleSubmit(event)}>
+            Calculate
+          </button>}
+
+        {showInputs &&
+          <button
+            className="btn text-white px-8 flex justify-center w-[60vw] max-w-[300px]"
+            onClick={(event) => resetState(event)}>
+            Reset values
+          </button>}
 
       </form>
-      {/* text */}
-      <div className="mt-24 py-6 rounded-2xl border-2 border-indigo-600">
-        <AnalysisResult />
-        
-      </div>
+
+      {/* analysis result */}
+      {analyse && <div className="mt-24 py-6 rounded-2xl border-2 border-indigo-600">
+        <AnalysisResult principal = {principal} interest = {interest} period = {period} invType={invType} invObjective = {invObjective}/>
+      </div>}
+      
     </div>
   );
 };
