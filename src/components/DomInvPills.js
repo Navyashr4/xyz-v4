f095e154b26a82d4bc8267368a2e72c6e11bd407
Navya Shree <navyashr4@gmail.com>
2022-11-06 16:02:19 +0530

    fixing dom pills dataaos

--- src/components/DomInvPills.js
+++ src/components/DomInvPills.js
@@ -3,14 +3,17 @@ import React from 'react'
 function DomInvPills({handleInvTypeApp}) {
   return (
         <section className="mt-6">
-          <div className="container mx-auto px-6 lg:px-0">
+          <div 
+            data-aos="fade-down"
+            data-aps-delay="700"
+            className="container mx-auto px-6 lg:px-0">
             {/* <div className="flex flex-col gap-y-5 items-center"> */}
               <div className='mx-auto flex gap-x-2 flex-wrap gap-y-4 justify-center lg:max-w-[40%]'>
                 <button
                     className="btn gap-x-6 text-sm lg:order-0 lg:h-16 lg:text-base text-center
                     bg-darkblue border-2 border-white hover:bg-blue"
-                    data-aos="fade-down"
-                    data-aps-delay="700"
+                    // data-aos="fade-down"
+                    // data-aps-delay="700"
                     onClick={()=>handleInvTypeApp("Bank Fixed Deposit")}
                 >
                     Bank Fixed Deposit
@@ -18,8 +21,8 @@ function DomInvPills({handleInvTypeApp}) {
                 <button
                     className="btn gap-x-6 px-6 lg:order-1 lg:px-10 text-sm lg:h-16 lg:text-base text-center
                     bg-darkblue border-2 border-white hover:bg-blue"
-                    data-aos="fade-down"
-                    data-aps-delay="700"
+                    // data-aos="fade-down"
+                    // data-aps-delay="700"
                     onClick={()=>handleInvTypeApp("Gold")}
                 >
                     Gold
@@ -27,8 +30,6 @@ function DomInvPills({handleInvTypeApp}) {
                 <button
                     className="order-4 lg:order-4 btn gap-x-6 px-6 lg:px-10 text-sm lg:h-16 lg:text-base text-center 
                     bg-darkblue border-2 border-white hover:bg-blue"
-                    data-aos="fade-down"
-                    data-aps-delay="700"
                     onClick={()=>handleInvTypeApp("ULIP")}
                 >
                     ULIP 
@@ -36,8 +37,8 @@ function DomInvPills({handleInvTypeApp}) {
                 <button
                     className="order-2 lg:order-2 btn gap-x-6 text-sm lg:h-16 lg:text-base text-center
                     bg-darkblue border-2 border-white hover:bg-blue"
-                    data-aos="fade-down"
-                    data-aps-delay="700"
+                    // data-aos="fade-down"
+                    // data-aps-delay="700"
                     onClick={()=>handleInvTypeApp("Real Estate")}
                 >
                     Real Estate
@@ -45,8 +46,8 @@ function DomInvPills({handleInvTypeApp}) {
                 <button
                     className="btn gap-x-6 px-8 lg:order-3 text-sm lg:h-16 lg:text-base text-center
                     bg-darkblue border-2 border-white hover:bg-blue"
-                    data-aos="fade-down"
-                    data-aps-delay="700"
+                    // data-aos="fade-down"
+                    // data-aps-delay="700"
                     onClick={()=>handleInvTypeApp("Public Provident Fund")}
                 >
                     Public Provident Fund
