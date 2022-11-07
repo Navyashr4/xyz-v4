3f092b39e67bbd4a6d9d9d85179603ecab06c7c0
Navya Shree <navyashr4@gmail.com>
2022-11-07 11:57:00 +0530

    text center for resources

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -8,12 +8,12 @@ const Resources = ({ invType }) => {
     <section className="pt-12 lg:pt-24 mx-4 lg:mx-0">
       <div className="container mx-auto">
         <div className="text-center max-w-[758px] mx-auto mb-24">
-          <p className="section-title text-white section-title md:text-3xl text-left max-w-[622px] mx-auto">
+          <p className="section-title text-white section-title md:text-3xl text-center max-w-[622px] mx-auto">
             Further resources to learn about {invType.toLowerCase()} investments
           </p>
           {resourcesArray.map((resource) => {
             return (
-              <div className="section-subtitle text-left lg:text-lg mb-4 max-w-[622px] mx-auto text-blue">
+              <div className="section-subtitle text-center lg:text-lg mb-4 max-w-[622px] mx-auto text-blue">
                 <a
                   href={resource.link}
                   target="_blank"
